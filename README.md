8faeacf1da2b50c6fe256a0bbd16860f71f6161c
Mike Erickson <codedungeon@gmail.com>
2016-11-17 18:49:56 -0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# pkg-version-cli
+
+## Package Version CLI
+Simple CLI to return package.json version information for current directory
+
+## Credits
+
+Install
+
+```
+$ npm i -g pkg-verison-cli
+```
+
+Usage
+
+```
+$ package-version
+Current Version: #.#.#
+```
+
+pkg-version-cli written by Mike Erickson
+
+E-Mail: [codedungeon@gmail.com](mailto:codedungeon@gmail.com)
+
+Twitter: [@codedungeon](http://twitter.com/codedungeon)
+
+Website: [codedungeon.org](http://codedungeon.org)
---
+++ cli.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+'use strict';
+const meow  = require('meow');
+const chalk = require('chalk');
+const fs    = require('fs');
+
+const cli = meow(`
+	Usage
+	  $ package-version
+
+	Example
+	  $ package-version
+	  0.0.1
+`);
+
+const filename = process.env.PWD + '/package.json'
+if (fs.existsSync(filename)) {
+  const pkgInfo = require(filename);
+  console.log(`${chalk.green.bold(pkgInfo.name)} ${chalk.white.bold('v' + pkgInfo.version)}`);
+}
+else {
+  console.error(chalk.red('Unable to locate valid `package.json`'));
+}
