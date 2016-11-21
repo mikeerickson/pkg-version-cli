0ec32eba655f158a0069998f673b68faf6630e77
Mike Erickson <merickson@cirruspath.com>
2016-11-21 11:26:15 -0800

    001 fix invalid name or version properties

--- cli.js
+++ cli.js
@@ -14,10 +14,18 @@ const cli = meow(`
 `);
 
 const filename = process.env.PWD + '/package.json'
+console.log('')
 if (fs.existsSync(filename)) {
   const pkgInfo = require(filename);
-  console.log(`${chalk.green.bold(pkgInfo.name)} ${chalk.white.bold('v' + pkgInfo.version)}`);
+	if ((pkgInfo.name) && (pkgInfo.version)) {
+    console.log(chalk.white.bold('==> Package Information'));
+		console.log(`    ${chalk.green.bold(pkgInfo.name)} ${chalk.white.bold('v' + pkgInfo.version)}`);
+	}
+	else {
+		console.error(chalk.red.bold(`==> Invalid ${chalk.white('name')} or ${chalk.white('version')} properties`));
+	}
 }
 else {
   console.error(chalk.red('Unable to locate valid `package.json`'));
 }
+console.log('')
