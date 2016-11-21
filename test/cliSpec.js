9147d4d4a304dc3a79297b5f28944acad8f36313
Mike Erickson <merickson@cirruspath.com>
2016-11-21 11:41:34 -0800

    updated spec to reflect output changes

--- test/cliSpec.js
+++ test/cliSpec.js
@@ -1,13 +1,15 @@
 let childProcess = require('child_process');
-let assert       = require('assert');
 var pkgInfo      = require('../package.json');
+var chai         = require('chai')
+
+var expect = chai.expect;
 
 describe('pkg-version-cli', () => {
 	it('should execute CLI without error', (done) => {
 		childProcess.execFile('./cli.js',[],{cwd: process.env.PWD}, (error, stdout) => {
 			if (error) { throw error }
       let compare = pkgInfo.name + ' v' + pkgInfo.version + '\n';
-			assert(stdout === compare);
+			expect(stdout).to.contain(compare);
 			done();
 		})
 	});
