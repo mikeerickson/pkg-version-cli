7eb0ebb1fcf327aadaf92e4dc31b7d0e6616da3d
Mike Erickson <merickson@cirruspath.com>
2016-11-22 13:18:25 -0800

    fixed workflow tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,44 @@
 const gulp   = require('gulp');
 const eslint = require('gulp-eslint');
 const mocha  = require('gulp-mocha');
+const msg    = require('gulp-messenger');
+const config = require('./gulp.config.js');
+const todo   = require('gulp-todo');
 
 gulp.task('eslint', () => {
-  return gulp.src(['./**/*.js','!node_modules/**'])
+  return gulp.src(config.scripts.src)
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
 });
 
-gulp.task('tdd', () =>
-    gulp.src('./test/**/*.js')
-        .pipe(mocha())
-        .once('error', () => {
-            process.exit(1);
-        })
-        .once('end', () => {
-            process.exit();
-        })
+gulp.task('test', () =>
+  gulp.src(config.test.src, {read: false})
+    .pipe(mocha())
+    .once('error', () => {
+      msg.error(msg.chalk.bold('==> Testing Failed...'));
+    })
+    .once('end', () => {
+      msg.log(msg.chalk.green.bold('==> Testing Completed Successfully...'));
+    })
 );
-gulp.task('watch:scripts', ['eslint','tdd'], () => {
-  gulp.watch('./**/*.js', ['eslint','tdd']);
+
+gulp.task('todo', () =>{
+  gulp.src(config.scripts.src)
+    .pipe(todo())
+    .pipe(gulp.dest('./'));
 });
+
+gulp.task('watch:eslint', ['eslint'], () => {
+  msg.note('==> Watching ESLint Scripts');
+  gulp.watch('./**/*.js', ['eslint']);
+});
+
+gulp.task('watch:test', ['test'], () => {
+  msg.note('==> Watching Test Scripts');
+  gulp.watch('./**/*.js', ['test']);
+});
+
+gulp.task('watch', ['watch:eslint','watch:test']);
+
+gulp.task('default',['eslint','test']);
--- test/cliSpec.js
+++ test/cliSpec.js
@@ -8,21 +8,25 @@ const expect = chai.expect;
 describe(chalk.cyan.bold('==> pkg-version-cli'), () => {
 
 	const filename = './cli.js';
-	it('should execute CLI without error', () => {
+	it('should execute CLI without error', (done) => {
 		childProcess.execFile(filename,[],{cwd: process.env.PWD}, (error, stdout) => {
 			if (error) { throw error; }
       let compare = pkgInfo.name + ' v' + pkgInfo.version;
 			expect(stdout).to.contain(compare);
+      done();
 		});
-
 	});
 
-	it('should execute CLI and return error (invalid filename)', () => {
+	it('should execute CLI and return error (invalid filename)', (done) => {
 		childProcess.execFile(filename,['packke.json'],{cwd: process.env.PWD}, (error, stdout) => {
 			if (error) { throw error; }
 			let compare = pkgInfo.name + ' v' + pkgInfo.version;
 			expect(stdout).to.contain('Unable to locate valid packke.json');
+      done();
 		});
-	});
+	}).timeout(3000);
 
 });
+
+
+// TODO: Test todo
